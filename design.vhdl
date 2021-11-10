2bbf6aacbd374a5d2663a5099a9ce4d2166a1b7e
Joel-alt <62847715+Joel-alt@users.noreply.github.com>
2021-11-10 15:53:32 +0100

    Update design.vhdl

--- design.vhdl
+++ design.vhdl
@@ -10,6 +10,8 @@ port (
     B : in std_logic_vector (3 downto 0);
     sel_FCT : in std_logic;
     S : out std_logic_vector (7 downto 0);
+    SR_IN_L : out std_logic;
+    SR_IN_R : out std_logic;
     SR_OUT_L : out std_logic;
     SR_OUT_R : out std_logic;
 );
@@ -63,27 +65,33 @@ begin
         SR_OUT_L <= '0';
         SR_OUT_R <= '0';
       when '1000' =>
-      	S (2 downto 0) <= A;
-        S (7 downto 3) <= (others => '0')
-        SR_OUT_L <= A(downto 0);
-        SR_OUT_R <= '0';
+      	S(3) <= SR_IN_L
+        S(2 downto 0) <= A(3 downto 1)
+        S (7 downto 4) <= (others => '0')
+        SR_OUT_R <= A(0)
       when '1001' =>
-      	S (2 downto 0) <= A;
-        S (7 downto 3) <= (others => '0')
-        SR_OUT_L <= A(downto 0);
-        SR_OUT_R <= '0';
+      	S(0) <= SR_IN_R
+      	S (3 downto 1) <= A(2 downto 0);
+        S (7 downto 4) <= (others => '0')
+        SR_OUT_L <= A(3);
       when '1010' =>
-      
+      	S(3) <= SR_IN_L
+        S(2 downto 0) <= B(3 downto 1)
+        S (7 downto 4) <= (others => '0')
+        SR_OUT_R <= B(0)
       when '1011' =>
-      
+      	S(0) <= SR_IN_R
+      	S (3 downto 1) <= B(2 downto 0);
+        S (7 downto 4) <= (others => '0')
+        SR_OUT_L <= B(3);
       when '1100' =>
-      
+      	
       when '1101' =>
-      
+      	
       when '1110' =>
       
       when '1111' =>
-      
+      	
     end process;
     
 end UAL_DataFlow;
