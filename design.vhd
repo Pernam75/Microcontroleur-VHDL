8e7ffce656917bf49ab270200ab89277a2f43b12
Bardane <62847653+Bardane@users.noreply.github.com>
2021-11-16 12:15:00 +0100

    Update design.vhd

--- design.vhd
+++ design.vhd
@@ -1,9 +1,8 @@
 -- Code your design here
 library IEEE;
 use IEEE.std_logic_1164.all;
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_signed.all;
 
 Entity TP_Coeur is
 port (
@@ -34,100 +33,106 @@ begin
     begin
       case SEL_FCT is
           when "0000" => 
-              S <= 0;
-              SR_OUT_L <= '0';
-              SR_OUT_R <= '0';
-              S(7 downto 0) <= (others => '0');
+              My_SR_OUT_L <= '0';
+              My_SR_OUT_R <= '0';
+              My_S(7 downto 0) <= (others => '0');
           when "0001" =>
-              S(7 downto 4) <= (others => '0');
-              S(3 downto 0) <= A;
-              SR_OUT_L <= '0' ;
-              SR_OUT_R <= '0' ;
+              My_S(7 downto 4) <= (others => '0');
+              My_S(3 downto 0) <= A;
+              My_SR_OUT_L <= '0' ;
+              My_SR_OUT_R <= '0' ;
           when "0010" =>
-              S(7 downto 4) <= (others => '0');
-              S(3 downto 0) <= B;
-              SR_OUT_L <= '0' ;
-              SR_OUT_R <= '0' ;
+              My_S(7 downto 4) <= (others => '0');
+              My_S(3 downto 0) <= B;
+              My_SR_OUT_L <= '0' ;
+              My_SR_OUT_R <= '0' ;
           when "0011" =>
-              S(7 downto 4) <= (others => '0');
-              S(3 downto 0) <= not A;
-              SR_OUT_L <= '0' ;
-              SR_OUT_R <= '0' ;
+              My_S(7 downto 4) <= (others => '0');
+              My_S(3 downto 0) <= not A;
+              My_SR_OUT_L <= '0' ;
+              My_SR_OUT_R <= '0' ;
           when "0100" =>
-              S(7 downto 4) <= (others => '0');
-              S(3 downto 0) <= not B;
-              SR_OUT_L <= '0' ;
-              SR_OUT_R <= '0' ;
+              My_S(7 downto 4) <= (others => '0');
+              My_S(3 downto 0) <= not B;
+              My_SR_OUT_L <= '0' ;
+              My_SR_OUT_R <= '0' ;
           when "0101" =>
-              S(7 downto 4) <= (others => '0');
-              S(3 downto 0) <= A and B;
-              SR_OUT_L <= '0' ;
-              SR_OUT_R <= '0' ;
+              My_S(7 downto 4) <= (others => '0');
+              My_S(3 downto 0) <= A and B;
+              My_SR_OUT_L <= '0' ;
+              My_SR_OUT_R <= '0' ;
           when "0110" =>
-              S(7 downto 4) <= (others => '0');
-              S(3 downto 0) <= A or B;
-              SR_OUT_L <= '0' ;
-              SR_OUT_R <= '0' ;
+              My_S(7 downto 4) <= (others => '0');
+              My_S(3 downto 0) <= A or B;
+              My_SR_OUT_L <= '0' ;
+              My_SR_OUT_R <= '0' ;
           when "0111" =>
-              S(7 downto 4) <= (others => '0');
-              S(3 downto 0) <= A xor B;
-              SR_OUT_L <= '0' ;
-              SR_OUT_R <= '0' ;
+              My_S(7 downto 4) <= (others => '0');
+              My_S(3 downto 0) <= A xor B;
+              My_SR_OUT_L <= '0' ;
+              My_SR_OUT_R <= '0' ;
           when "1000" =>
-              S(3) <= SR_IN_L ;
-              S(2 downto 0) <= A(3 downto 1) ;
-              S(7 downto 4) <= (others => '0') ;
-              SR_OUT_R <= A(0) ;
+              My_S(3) <= SR_IN_L ;
+              My_S(2 downto 0) <= A(3 downto 1) ;
+              My_S(7 downto 4) <= (others => '0') ;
+              My_SR_OUT_R <= A(0) ;
           when "1001" =>
-              S(0) <= SR_IN_R ;
-              S (3 downto 1) <= A(2 downto 0);
-              S (7 downto 4) <= (others => '0');
-              SR_OUT_L <= A(3);
+              My_S(0) <= SR_IN_R ;
+              My_S (3 downto 1) <= A(2 downto 0);
+              My_S (7 downto 4) <= (others => '0');
+              My_SR_OUT_L <= A(3);
           when "1010" =>
-              S(3) <= SR_IN_L ;
-              S(2 downto 0) <= B(3 downto 1) ;
-              S(7 downto 4) <= (others => '0') ;
-              SR_OUT_R <= B(0) ;
+              My_S(3) <= SR_IN_L ;
+              My_S(2 downto 0) <= B(3 downto 1) ;
+              My_S(7 downto 4) <= (others => '0') ;
+              My_SR_OUT_R <= B(0) ;
           when "1011" =>
-              S(0) <= SR_IN_R ;
-              S (3 downto 1) <= B(2 downto 0);
-              S (7 downto 4) <= (others => '0');
-              SR_OUT_L <= B(3);
+              My_S(0) <= SR_IN_R ;
+              My_S (3 downto 1) <= B(2 downto 0);
+              My_S (7 downto 4) <= (others => '0');
+              My_SR_OUT_L <= B(3);
+              My_SR_OUT_R <= '0';
           when "1100" =>
-              My_A_VAR(3 downto 0) <= A;
-          	  My_B_VAR(3 downto 0) <= B;
+              My_A_VAR(3 downto 0) <= My_A;
+          	  My_B_VAR(3 downto 0) <= My_B;
               My_S_VAR <= My_A_VAR + My_B_VAR ;
               My_S_VAR <= My_A_VAR + ("0000000" & SR_IN_R) ;
-              S <= My_S_VAR ;
-              SR_OUT_L <= '0';
-              SR_OUT_R <= '0';
+              My_S <= My_S_VAR ;
+              My_SR_OUT_L <= '0';
+              My_SR_OUT_R <= '0';
           when "1101" =>
-              My_A_VAR(3 downto 0) <= A;
-              My_B_VAR(3 downto 0) <= B;
+              My_A_VAR(3 downto 0) <= My_A;
+              My_B_VAR(3 downto 0) <= My_B;
               My_A_VAR(7 downto 4) <= (others => A(3));
               My_B_VAR(7 downto 4) <= (others => B(3));
               My_S_VAR <= My_A_VAR + My_B_VAR ;
-              S <= My_S_VAR ;
-              SR_OUT_L <= '0';
-              SR_OUT_R <= '0';
+              My_S <= My_S_VAR ;
+              My_SR_OUT_L <= '0';
+              My_SR_OUT_R <= '0';
           when "1110" =>
-              My_A_VAR(3 downto 0) <= A;
-              My_B_VAR(3 downto 0) <= B;
+              My_A_VAR(3 downto 0) <= My_A;
+              My_B_VAR(3 downto 0) <= My_B;
               My_A_VAR(7 downto 4) <= (others => A(3));
               My_B_VAR(7 downto 4) <= (others => B(3));
               My_S_VAR <= My_A_VAR - My_B_VAR ;
-              S <= My_S_VAR ;
-              SR_OUT_L <= '0';
-              SR_OUT_R <= '0';
-          when "1111" =>
-              S <= A * B;
-              SR_OUT_L <= '0';
-              SR_OUT_R <= '0';
-
+              My_S <= My_S_VAR ;
+              My_SR_OUT_L <= '0';
+              My_SR_OUT_R <= '0';
+          when others =>
+              My_S <= My_A * My_B;
+              My_SR_OUT_L <= '0';
+              My_SR_OUT_R <= '0';
          end case;
        
 end process;
-
+My_A <= A;
+My_B <= B;
+My_SR_IN_L <= SR_IN_L;
+My_SR_IN_R <= SR_IN_R;
+    
+S <= My_S;
+SR_OUT_L <= My_SR_OUT_L;
+SR_OUT_R <= My_SR_OUT_R;
 
 end;
             
