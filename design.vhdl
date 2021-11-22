5c24b7568c9db28e9556690ab9270a18437ad96e
Pernam75 <58521821+Pernam75@users.noreply.github.com>
2021-11-22 14:22:17 +0100

    ptit push

--- design.vhdl
+++ design.vhdl
@@ -1,6 +1,7 @@
 -- Code your design here
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 use IEEE.std_logic_signed.all;
 
 entity UAL is
@@ -19,10 +20,10 @@ end UAL;
 
 architecture UAL_DataFlow of UAL is 
 	
-    signal My_A : std_logic_vector (7 downto 0);
-    signal My_B : std_logic_vector (7 downto 0);
-    signal My_SR_IN_L : std_logic_vector (7 downto 0);
-    signal My_SR_IN_R : std_logic_vector (7 downto 0);
+    signal My_A : std_logic_vector (3 downto 0);
+    signal My_B : std_logic_vector (3 downto 0);
+    signal My_SR_IN_L : std_logic;
+    signal My_SR_IN_R : std_logic;
     signal My_SR_OUT_L : std_logic;
     signal My_SR_OUT_R : std_logic;
     signal My_S : std_logic_vector (7 downto 0);
@@ -31,7 +32,9 @@ architecture UAL_DataFlow of UAL is
 begin
 	
     -- process explicite - intstructions séquentielle
-    UALProc : process (sel_FCT)
+    UALProc : process (A, B, SR_IN_L, SR_IN_R, sel_FCT)
+
+    variable My_A_var, My_B_var, My_S_var: std_logic_vector (7 downto 0);
     begin
       case sel_FCT is
       when "0000" =>
@@ -78,21 +81,25 @@ begin
         My_S(2 downto 0) <= A(3 downto 1);
         My_S (7 downto 4) <= (others => '0');
         My_SR_OUT_R <= A(0);
+        My_SR_OUT_L <= '0';
       when "1001" =>
         My_S(0) <= SR_IN_R;
         My_S (3 downto 1) <= A(2 downto 0);
         My_S (7 downto 4) <= (others => '0');
         My_SR_OUT_L <= A(3);
+        My_SR_OUT_R <= '0';
       when "1010" =>
         My_S(3) <= SR_IN_L;
         My_S(2 downto 0) <= B(3 downto 1);
         My_S (7 downto 4) <= (others => '0');
         My_SR_OUT_R <= B(0);
+        My_SR_OUT_L <= '0';
       when "1011" =>
         My_S(0) <= SR_IN_R;
         My_S (3 downto 1) <= B(2 downto 0);
         My_S (7 downto 4) <= (others => '0');
         My_SR_OUT_L <= B(3);
+        My_SR_OUT_R <= '0';
       when "1100" =>
         My_S <= My_A + My_B + My_SR_In_R;
         My_SR_OUT_L <= '0';
