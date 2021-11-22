822b3bf150e30e19feb4c221d5b35ec7f68ca77b
Frin M <72406720+Mfrin@users.noreply.github.com>
2021-11-22 13:24:03 +0100

    Update designbuffers.vhd

--- UAL/designbuffers.vhd
+++ UAL/designbuffers.vhd
@@ -39,9 +39,57 @@ architecture Buffers_arch of Buffers is
 begin
 	BuffA_process : process (reset,clk,BuffA_val)
     begin 
- 		if reset ='1' then
+ 	if reset ='1' then
         	BuffA_out <= (other => '0');
         else if (rising_edge (clk) and BuffA_val ='0') then
         	BuffA_out <= BuffA_in;
     	end if;
     end process;
+    
+    
+    BuffB_process : process (reset,clk,BuffB_val)
+    begin 
+ 	if reset ='1' then
+        	BuffB_out <= (other => '0');
+        else if (rising_edge (clk) and BuffB_val ='0') then
+        	BuffB_out <= BuffB_in;
+    	end if;
+    end process;
+    
+    MEM1_process : process (reset,clk,MEM1_val)
+    begin 
+ 	if reset ='1' then
+        	MEM1_out <= (other => '0');
+        else if (rising_edge(clk) and MEM1_val ='0') then
+        	MEM1_out <= MEM1_in;
+    	end if;
+    end process;
+    
+    MEM2_process : process (reset,clk,MEM2_val)
+    begin 
+ 	if reset ='1' then
+        	MEM2_out <= (other => '0');
+        else if (rising_edge(clk) and MEM2_val ='0') then
+        	MEM2_out <= MEM2_in;
+    	end if;
+    end process;
+    
+    SR_L_process : process (reset,clk,SR_L_val)
+    begin 
+ 	if reset ='1' then
+        	SR_L_out <= (other => '0');
+        else if (rising_edge(clk) and SR_L_val ='0') then
+        	SR_L_out <= SR_L_in;
+    	end if;
+    end process;
+    
+    SR_R_process : process (reset,clk,SR_R_val)
+    begin 
+ 	if reset ='1' then
+        	SR_R_out <= (other => '0');
+        else if (rising_edge(clk) and SR_R_val ='0') then
+        	SR_R_out <= SR_R_in;
+    	end if;
+    end process;
+        
+end architecture;
