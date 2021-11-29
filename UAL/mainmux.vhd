a44620f0ac91fe7a7a83ba4f46bdf0911682b3c1
Bardane <62847653+Bardane@users.noreply.github.com>
2021-11-29 17:07:40 +0100

    Create mainmux.vhd

---
+++ UAL/mainmux.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity MainMux is port(
+    E0, E1, E2, E3, SEL0, SEL1: in STD_LOGIC;
+    S: out STD_LOGIC);
+end;
+
+architecture MainMux_Arch of MainMux is
+
+begin
+
+    with SEL1 & SEL0 select
+        S <= E0 when "00",
+             E1 when "01",
+             E2 when "10",
+             E3 when others;
+             
+end FLOT_MUX;
