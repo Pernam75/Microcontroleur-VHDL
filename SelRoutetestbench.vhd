ac5bc8765cc6be272ab039d1ba82bd8ab861a7a9
Jules <jules.rubin@efrei.net>
2021-12-04 23:14:54 +0100

    LE COMMIT DE L'AMBIANCE

--- SelRoutetestbench.vhd
+++ SelRoutetestbench.vhd
@@ -10,17 +10,22 @@ architecture mySEL_ROUTEtestbench_Arch of mySEL_ROUTEtestbench is
 
 	-- Déclaration du composant à tester -> renvoie vers l'entité ET2 !
     component SelRouteUAL is
-    port (
-        SEL_ROUTE, A, B, Buffer_A_OUT, Buffer_B_OUT, Buffer_A_IN, Buffer_B_IN : in std_logic_vector (3 downto 0);
-        Buffer_Memory1_OUT, Buffer_Memory2_OUT, Buffer_Memory1_IN, Buffer_Memory2_IN : out STD_LOGIC_VECTOR (7 downto 0);
-        S : in std_logic_vector(7 downto 0);
-        CE_Buffer_A, CE_Buffer_B, CE_Buffer_Memory1, CE_Buffer_Memory2 : out std_logic
+    Port ( 
+            S : in STD_LOGIC_VECTOR (7 downto 0);
+            SEL_ROUTE, A, B : in STD_LOGIC_VECTOR (3 downto 0);
+            Buffer_A_OUT, Buffer_B_OUT : out STD_LOGIC_VECTOR (3 downto 0);
+            Buffer_A_IN, Buffer_B_IN : in std_logic_vector(3 downto 0);
+            Buffer_Memory1_OUT, Buffer_Memory2_OUT : out STD_LOGIC_VECTOR (7 downto 0);
+            Buffer_Memory1_IN, Buffer_Memory2_IN : in STD_LOGIC_VECTOR (7 downto 0);
+            CE_Buffer_A, CE_Buffer_B, CE_Buffer_Memory1, CE_Buffer_Memory2 : out STD_LOGIC            
     );
     end component;
     -- Déclaration des signaux internes à l'architecture pour résilier les simulations 
-    signal A_sim, B_sim, Buffer_A_OUT_sim, Buffer_B_OUT_sim, Buffer_A_IN_sim, Buffer_B_IN_sim, SEL_ROUTE_sim : std_logic_vector(3 downto 0);
-    signal S_sim, Buffer_Memory1_OUT_sim, Buffer_Memory2_OUT_sim, Buffer_Memory1_IN_sim, Buffer_Memory2_IN_sim : std_logic_vector(7 downto 0);
-    signal CE_Buffer_A_sim, CE_Buffer_B_sim, CE_Buffer_Memory1_sim, CE_Buffer_Memory2_sim : std_logic := '0';
+    signal S_sim : STD_LOGIC_VECTOR (7 downto 0);
+    signal SEL_ROUTE_sim, A_sim, B_sim : STD_LOGIC_VECTOR (3 downto 0);
+    signal Buffer_A_OUT_sim, Buffer_B_OUT_sim, Buffer_A_IN_sim, Buffer_B_IN_sim : STD_LOGIC_VECTOR (3 downto 0);
+    signal Buffer_Memory1_OUT_sim, Buffer_Memory2_OUT_sim, Buffer_Memory1_IN_sim, Buffer_Memory2_IN_sim : STD_LOGIC_VECTOR (7 downto 0);
+    signal CE_Buffer_A_sim, CE_Buffer_B_sim, CE_Buffer_Memory1_sim, CE_Buffer_Memory2_sim : STD_LOGIC;
     
 begin 
     MyComponentSEL_ROUTEunderTest : SelRouteUAL
@@ -46,18 +51,17 @@ begin
     
     MyStimulus_Proc : process
     begin
+      CE_Buffer_A_sim <= '0';
+      CE_Buffer_B_sim <= '0';
+      CE_Buffer_Memory1_sim <= '0';
+      CE_Buffer_Memory2_sim <= '0';
+      S_sim <= (others => '0');
+      Buffer_Memory1_OUT_sim <= (others => '0');
+      Buffer_Memory2_OUT_sim <= (others => '0');
+      Buffer_Memory1_IN_sim <= (others => '0');
+      Buffer_Memory2_IN_sim <= (others => '0');
         for i in 0 to 15 loop
         	SEL_ROUTE_sim <= std_logic_vector(to_unsigned(i,4));
-            SEL_ROUTE_sim <= (others => '0');
-            CE_Buffer_A_sim <= '0';
-            CE_Buffer_B_sim <= '0';
-            CE_Buffer_Memory1_sim <= '0';
-            CE_Buffer_Memory2_sim <= '0';
-            S_sim <= (others => '0');
-            Buffer_Memory1_OUT_sim <= (others => '0');
-            Buffer_Memory2_OUT_sim <= (others => '0');
-            Buffer_Memory1_IN_sim <= (others => '0');
-            Buffer_Memory2_IN_sim <= (others => '0');
             wait for 100 us;
             report std_logic'image(SEL_ROUTE_sim(3)) & std_logic'image(SEL_ROUTE_sim(2)) & std_logic'image(SEL_ROUTE_sim(1)) & std_logic'image(SEL_ROUTE_sim(0));
             report "CE_Buffer_A : "&std_logic'image(CE_Buffer_A_sim)&" ; CE_Buffer_B : "&std_logic'image(CE_Buffer_B_sim) & " ; CE_Buffer_Memory1 : " & std_logic'image(CE_Buffer_Memory1_sim) & " ; CE_Buffer_Memory2 : " & std_logic'image(CE_Buffer_Memory2_sim);
--- designSelRoute.vhd
+++ designSelRoute.vhd
@@ -5,8 +5,10 @@ entity SelRouteUAL is
     Port ( 
             S : in STD_LOGIC_VECTOR (7 downto 0);
             SEL_ROUTE, A, B : in STD_LOGIC_VECTOR (3 downto 0);
-            Buffer_A_OUT, Buffer_B_OUT, Buffer_A_IN, Buffer_B_IN : in STD_LOGIC_VECTOR (3 downto 0);
-            Buffer_Memory1_OUT, Buffer_Memory2_OUT, Buffer_Memory1_IN, Buffer_Memory2_IN : out STD_LOGIC_VECTOR (7 downto 0);
+            Buffer_A_IN, Buffer_B_IN : in STD_LOGIC_VECTOR (3 downto 0);
+            Buffer_A_OUT, Buffer_B_OUT : out std_logic_vector(3 downto 0);
+            Buffer_Memory1_OUT, Buffer_Memory2_OUT : out STD_LOGIC_VECTOR (7 downto 0);
+            Buffer_Memory1_IN, Buffer_Memory2_IN : in std_logic_vector(7 downto 0);
             CE_Buffer_A, CE_Buffer_B, CE_Buffer_Memory1, CE_Buffer_Memory2 : out STD_LOGIC            
     );
 end SelRouteUAL;
@@ -15,7 +17,7 @@ architecture SelRouteDataFlow of SelRouteUAL is
 
 begin
 
-    MySelRouteProc : process (SEL_ROUTE, S, A, B, Buffer_A_OUT, Buffer_B_OUT, Buffer_Memory1_OUT, Buffer_Memory2_OUT)
+    MySelRouteProc : process (SEL_ROUTE, S, A, B, Buffer_A_IN, Buffer_B_IN, Buffer_Memory1_IN, Buffer_Memory2_IN)
     begin
         case SEL_ROUTE is
             when "0000" =>
@@ -23,160 +25,160 @@ begin
                 CE_Buffer_B <= '0';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= A;
-                Buffer_B_IN <= (others => '0');
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= A;
+                Buffer_B_OUT <= (others => '0');
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "0001" =>
                 CE_Buffer_A <= '1';
                 CE_Buffer_B <= '0';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= Buffer_Memory1_OUT(3 downto 0);
-                Buffer_B_IN <= (others => '0');
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= Buffer_Memory1_IN(3 downto 0);
+                Buffer_B_OUT <= (others => '0');
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "0010" =>
                 CE_Buffer_A <= '1';
                 CE_Buffer_B <= '0';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= Buffer_Memory1_OUT(7 downto 4);
-                Buffer_B_IN <= (others => '0');
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= Buffer_Memory1_IN(7 downto 4);
+                Buffer_B_OUT <= (others => '0');
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
                 
             when "0011" =>
                 CE_Buffer_A <= '1';
                 CE_Buffer_B <= '0';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= Buffer_Memory2_OUT(3 downto 0);
-                Buffer_B_IN <= (others => '0');
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= Buffer_Memory2_IN(3 downto 0);
+                Buffer_B_OUT <= (others => '0');
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "0100" =>
                 CE_Buffer_A <= '1';
                 CE_Buffer_B <= '0';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= Buffer_Memory2_OUT(7 downto 4);
-                Buffer_B_IN <= (others => '0');
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= Buffer_Memory2_IN(7 downto 4);
+                Buffer_B_OUT <= (others => '0');
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "0101" =>
                 CE_Buffer_A <= '1';
                 CE_Buffer_B <= '0';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= S(3 downto 0);
-                Buffer_B_IN <= (others => '0');
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= S(3 downto 0);
+                Buffer_B_OUT <= (others => '0');
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "0110" =>
                 CE_Buffer_A <= '1';
                 CE_Buffer_B <= '0';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= S(7 downto 4);
-                Buffer_B_IN <= (others => '0');
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= S(7 downto 4);
+                Buffer_B_OUT <= (others => '0');
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "0111" =>
                 CE_Buffer_A <= '0';
                 CE_Buffer_B <= '1';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= (others => '0');
-                Buffer_B_IN <= B;
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= (others => '0');
+                Buffer_B_OUT <= B;
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "1000" =>
                 CE_Buffer_A <= '0';
                 CE_Buffer_B <= '1';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= (others => '0');
-                Buffer_B_IN <= Buffer_Memory1_OUT(3 downto 0);
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= (others => '0');
+                Buffer_B_OUT <= Buffer_Memory1_IN(3 downto 0);
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "1001" =>
                 CE_Buffer_A <= '0';
                 CE_Buffer_B <= '1';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= (others => '0');
-                Buffer_B_IN <= Buffer_Memory1_OUT(7 downto 4);
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= (others => '0');
+                Buffer_B_OUT <= Buffer_Memory1_IN(7 downto 4);
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
                 
             when "1010" =>
                 CE_Buffer_A <= '0';
                 CE_Buffer_B <= '1';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= (others => '0');
-                Buffer_B_IN <= Buffer_Memory2_OUT(3 downto 0);
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= (others => '0');
+                Buffer_B_OUT <= Buffer_Memory2_IN(3 downto 0);
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "1011" =>
                 CE_Buffer_A <= '0';
                 CE_Buffer_B <= '1';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= (others => '0');
-                Buffer_B_IN <= Buffer_Memory1_OUT(7 downto 4);
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= (others => '0');
+                Buffer_B_OUT <= Buffer_Memory1_IN(7 downto 4);
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "1100" =>
                 CE_Buffer_A <= '0';
                 CE_Buffer_B <= '1';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= (others => '0');
-                Buffer_B_IN <= S(3 downto 0);
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= (others => '0');
+                Buffer_B_OUT <= S(3 downto 0);
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "1101" =>
                 CE_Buffer_A <= '0';
                 CE_Buffer_B <= '1';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= (others => '0');
-                Buffer_B_IN <= S(7 downto 4);
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= (others => '0');
+                Buffer_B_OUT <= S(7 downto 4);
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= (others => '0');
             
             when "1110" =>
                 CE_Buffer_A <= '0';
                 CE_Buffer_B <= '0';
                 CE_Buffer_Memory1 <= '1';
                 CE_Buffer_Memory2 <= '0';
-                Buffer_A_IN <= (others => '0');
-                Buffer_B_IN <= (others => '0');
-                Buffer_Memory1_IN <= S;
-                Buffer_Memory2_IN <= (others => '0');
+                Buffer_A_OUT <= (others => '0');
+                Buffer_B_OUT <= (others => '0');
+                Buffer_Memory1_OUT <= S;
+                Buffer_Memory2_OUT <= (others => '0');
             
             when others =>
                 CE_Buffer_A <= '0';
                 CE_Buffer_B <= '0';
                 CE_Buffer_Memory1 <= '0';
                 CE_Buffer_Memory2 <= '1';
-                Buffer_A_IN <= (others => '0');
-                Buffer_B_IN <= (others => '0');
-                Buffer_Memory1_IN <= (others => '0');
-                Buffer_Memory2_IN <= S;
+                Buffer_A_OUT <= (others => '0');
+                Buffer_B_OUT <= (others => '0');
+                Buffer_Memory1_OUT <= (others => '0');
+                Buffer_Memory2_OUT <= S;
         end case;
     end process;
 
