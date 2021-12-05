f3efce61f293e700d74ce7be8722b397a2808052
Bardane <62847653+Bardane@users.noreply.github.com>
2021-12-05 23:32:25 +0100

    Update Design_Selroute.vhd

--- UAL/SELROUTE/Design_Selroute.vhd
+++ UAL/SELROUTE/Design_Selroute.vhd
@@ -4,179 +4,185 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity UAL_SELROUTE is
     Port ( 
-            SEL_ROUTE, A, B : in STD_LOGIC_VECTOR (3 downto 0);
+            SEL_ROUTE : in STD_LOGIC_VECTOR (3 downto 0);
             S : in STD_LOGIC_VECTOR (7 downto 0);
-            buff_A_out, buff_B_out : out STD_LOGIC_VECTOR (3 downto 0);
-            mem_1_out, mem_2_out : out STD_LOGIC_VECTOR (7 downto 0);
-            buff_A_in, buff_B_in : in STD_LOGIC_VECTOR (3 downto 0);
-            mem_1_in, mem_2_in : in STD_LOGIC_VECTOR (7 downto 0);
-            BuffA_val, BuffB_val : out STD_LOGIC;
-            mem1_val, mem2_val  : out STD_LOGIC           
+            A : in STD_LOGIC_VECTOR (3 downto 0);
+            B : in STD_LOGIC_VECTOR (3 downto 0);
+            Mem_1_out : in STD_LOGIC_VECTOR (7 downto 0);
+            Mem_2_out : in STD_LOGIC_VECTOR (7 downto 0);
+            BuffA_in : out STD_LOGIC_VECTOR (3 downto 0);
+            BuffB_in : out STD_LOGIC_VECTOR (3 downto 0);
+            Mem_1_in : out STD_LOGIC_VECTOR (7 downto 0);
+            Mem_2_in : out STD_LOGIC_VECTOR (7 downto 0);
+            BuffA_val : out STD_LOGIC;
+            BuffB_val : out STD_LOGIC;
+            Mem1_val : out STD_LOGIC;
+            Mem2_val : out STD_LOGIC            
     );
 end UAL_SELROUTE;
 
-architecture UAL_SELROUTE_Arch of UAL_SELROUTE is
+architecture UALSELROUTE_Arch of UAL_SELROUTE is
 
 begin
 
-    MySELROUTEProc : process (SEL_ROUTE, S, A, B, buff_A_in, buff_B_in, mem_1_in, mem_2_in)
+    MySelRouteProc : process (SEL_ROUTE, S, A, B, Mem_1_out, Mem_2_out)
     begin
         case SEL_ROUTE is
             when "0000" =>
-                buff_A_out <= A;
-                buff_B_out <= (others => '0');
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= A;
+                BuffB_in <= (others => '0');
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '1';
                 BuffB_val <= '0';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "0001" =>
-                buff_A_out <= mem_1_in(3 downto 0);
-                buff_B_out <= (others => '0');
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= Mem_1_out(3 downto 0);
+                BuffB_in <= (others => '0');
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '1';
                 BuffB_val <= '0';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "0010" =>
-                buff_A_out <= mem_1_in(7 downto 4);
-                buff_B_out <= (others => '0');
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= Mem_1_out(7 downto 4);
+                BuffB_in <= (others => '0');
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '1';
                 BuffB_val <= '0';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
                 
             when "0011" =>
-                buff_A_out <= mem_2_in(3 downto 0);
-                buff_B_out <= (others => '0');
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= Mem_2_out(3 downto 0);
+                BuffB_in <= (others => '0');
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '1';
                 BuffB_val <= '0';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "0100" =>
-                buff_A_out <= mem_2_in(7 downto 4);
-                buff_B_out <= (others => '0');
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= Mem_2_out(7 downto 4);
+                BuffB_in <= (others => '0');
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '1';
                 BuffB_val <= '0';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "0101" =>
-                buff_A_out <= S(3 downto 0);
-                buff_B_out <= (others => '0');
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= S(3 downto 0);
+                BuffB_in <= (others => '0');
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '1';
                 BuffB_val <= '0';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "0110" =>
-                buff_A_out <= S(7 downto 4);
-                buff_B_out <= (others => '0');
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= S(7 downto 4);
+                BuffB_in <= (others => '0');
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '1';
                 BuffB_val <= '0';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "0111" =>
-                buff_A_out <= (others => '0');
-                buff_B_out <= B;
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= (others => '0');
+                BuffB_in <= B;
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '0';
                 BuffB_val <= '1';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "1000" =>
-                buff_A_out <= (others => '0');
-                buff_B_out <= mem_1_in(3 downto 0);
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= (others => '0');
+                BuffB_in <= Mem_1_out(3 downto 0);
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '0';
                 BuffB_val <= '1';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "1001" =>
-                buff_A_out <= (others => '0');
-                buff_B_out <= mem_1_in(7 downto 4);
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= (others => '0');
+                BuffB_in <= Mem_1_out(7 downto 4);
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '0';
                 BuffB_val <= '1';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
                 
             when "1010" =>
-                buff_A_out <= (others => '0');
-                buff_B_out <= mem_2_in(3 downto 0);
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= (others => '0');
+                BuffB_in <= Mem_2_out(3 downto 0);
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '0';
                 BuffB_val <= '1';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "1011" =>
-                buff_A_out <= (others => '0');
-                buff_B_out <= mem_1_in(7 downto 4);
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= (others => '0');
+                BuffB_in <= Mem_1_out(7 downto 4);
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '0';
                 BuffB_val <= '1';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "1100" =>
-                buff_A_out <= (others => '0');
-                buff_B_out <= S(3 downto 0);
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= (others => '0');
+                BuffB_in <= S(3 downto 0);
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '0';
                 BuffB_val <= '1';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "1101" =>
-                buff_A_out <= (others => '0');
-                buff_B_out <= S(7 downto 4);
-                mem_1_out <= (others => '0');
-                mem_2_out <= (others => '0');
+                BuffA_in <= (others => '0');
+                BuffB_in <= S(7 downto 4);
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '0';
                 BuffB_val <= '1';
                 Mem1_val <= '0';
                 Mem2_val <= '0';
             
             when "1110" =>
-                buff_A_out <= (others => '0');
-                buff_B_out <= (others => '0');
-                mem_1_out <= S;
-                mem_2_out <= (others => '0');
+                BuffA_in <= (others => '0');
+                BuffB_in <= (others => '0');
+                Mem_1_in <= S;
+                Mem_2_in <= (others => '0');
                 BuffA_val <= '0';
                 BuffB_val <= '0';
                 Mem1_val <= '1';
                 Mem2_val <= '0';
             
             when others =>
-                buff_A_out <= (others => '0');
-                buff_B_out <= (others => '0');
-                mem_1_out <= (others => '0');
-                mem_2_out <= S;
+                BuffA_in <= (others => '0');
+                BuffB_in <= (others => '0');
+                Mem_1_in <= (others => '0');
+                Mem_2_in <= S;
                 BuffA_val <= '0';
                 BuffB_val <= '0';
                 Mem1_val <= '0';
@@ -184,4 +190,4 @@ begin
         end case;
     end process;
 
-end UAL_SELROUTE_Arch;
+end UALSELROUTE_Arch;
