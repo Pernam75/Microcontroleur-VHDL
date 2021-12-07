5bbcc7e24e65a23a1bd2a8e7e98c40a92fec6f66
Bardane <62847653+Bardane@users.noreply.github.com>
2021-12-07 16:27:01 +0100

    Update Design_Buffers.vhd

--- UAL/BUFFERS/Design_Buffers.vhd
+++ UAL/BUFFERS/Design_Buffers.vhd
@@ -1,4 +1,3 @@
--- Code your design here
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
@@ -8,14 +7,15 @@ Entity Buffers is
 port (
 	clk : in std_logic;
     reset : in std_logic;
+    preset : in std_logic;
     
-    Mem1_in : in std_logic_vector(7 downto 0);
-    Mem1_out : out std_logic_vector(7 downto 0);
-    Mem1_val : in std_logic;
+    mem1_in : in std_logic_vector(7 downto 0);
+    mem1_out : out std_logic_vector(7 downto 0);
+    mem1_val : in std_logic;
     
-    Mem2_in : in std_logic_vector(7 downto 0);
-    Mem2_out : out std_logic_vector(7 downto 0);
-    Mem2_val : in std_logic;
+    mem2_in : in std_logic_vector(7 downto 0);
+    mem2_out : out std_logic_vector(7 downto 0);
+    mem2_val : in std_logic;
     
     SR_L_in : in std_logic;
     SR_L_out : out std_logic;
@@ -23,7 +23,7 @@ port (
     
     SR_R_in : in std_logic;
     SR_R_out : out std_logic;
-    SR_L_val : in std_logic;
+    SR_R_val : in std_logic;
     
     BuffA_in : in std_logic_vector(3 downto 0);
     BuffA_out : out std_logic_vector(3 downto 0);
@@ -31,14 +31,13 @@ port (
     
     BuffB_in : in std_logic_vector(3 downto 0);
     BuffB_out : out std_logic_vector(3 downto 0);
-    BuffB_val : in std_logic
-	;
+    BuffB_val : in std_logic;
     
     Buff_SEL_FCT_in : in STD_LOGIC_VECTOR (3 downto 0);
     Buff_SEL_FCT_out : out STD_LOGIC_VECTOR (3 downto 0);
     
     Buff_SEL_OUT_in : in STD_LOGIC_VECTOR (1 downto 0);
-    Buff_SEL_OUT_out : out STD_LOGIC_VECTOR (1 downto 0)
+    Buff_SEL_OUT_out : out STD_LOGIC_VECTOR (1 downto 0));
     
     end Buffers;
     
@@ -48,8 +47,8 @@ begin
 	BuffA_process : process (reset,clk,BuffA_val)
     begin 
  		if (reset ='1') then
-        	BuffA_out <= (other => '0');
-        elsif (rising_edge(clk) and BuffA_val ='0') then
+        	BuffA_out <= (others => '0');
+        elsif (rising_edge(clk) and BuffA_val ='1') then
         	BuffA_out <= BuffA_in;
     	end if;
     end process;
@@ -58,35 +57,35 @@ begin
     BuffB_process : process (reset,clk,BuffB_val)
     begin 
  		if reset ='1' then
-        	BuffB_out <= (other => '0');
-        elsif (rising_edge(clk) and BuffB_val ='0') then
+        	BuffB_out <= (others => '0');
+        elsif (rising_edge(clk) and BuffB_val ='1') then
         	BuffB_out <= BuffB_in;
     	end if;
     end process;
     
-    MEM1_process : process (reset,clk,MEM1_val)
+    mem1_process : process (reset,clk,mem1_val)
     begin 
  		if reset ='1' then
-        	MEM1_out <= (other => '0');
-        elsif (rising_edge(clk) and MEM1_val ='0') then
-        	MEM1_out <= MEM1_in;
+        	mem1_out <= (others => '0');
+        elsif (rising_edge(clk) and mem1_val ='1') then
+        	mem1_out <= mem1_in;
     	end if;
     end process;
     
-    MEM2_process : process (reset,clk,MEM2_val)
+    mem2_process : process (reset,clk,mem2_val)
     begin 
  		if reset ='1' then
-        	MEM2_out <= (other => '0');
-        elsif (rising_edge(clk) and MEM2_val ='0') then
-        	MEM2_out <= MEM2_in;
+        	mem2_out <= (others => '0');
+        elsif (rising_edge(clk) and mem2_val ='1') then
+        	mem2_out <= mem2_in;
     	end if;
     end process;
     
     SR_L_process : process (reset,clk,SR_L_val)
     begin 
  		if reset ='1' then
-        	SR_L_out <= (other => '0');
-        elsif (rising_edge(clk) and SR_L_val ='0') then
+        	SR_L_out <= '0';
+        elsif (rising_edge(clk) and SR_L_val ='1') then
         	SR_L_out <= SR_L_in;
     	end if;
     end process;
@@ -94,27 +93,27 @@ begin
     SR_R_process : process (reset,clk,SR_R_val)
     begin 
  		if reset ='1' then
-        	SR_R_out <= (other => '0');
-        elsif (rising_edge(clk) and SR_R_val ='0') then
+        	SR_R_out <= '0';
+        elsif (rising_edge(clk) and SR_R_val ='1') then
         	SR_R_out <= SR_R_in;
     	end if;
     end process;
     
-    Buf_SEL_FCT_process : process (reset, clk)
+    Buff_SEL_FCT_process : process (reset, clk)
     begin
         if reset = '1' then
-            Buf_SEL_FCT_out <= (others => '0'); 
+            Buff_SEL_FCT_out <= (others => '0'); 
         elsif (rising_edge(clk)) then
-            Buf_SEL_FCT_out <= Buf_SEL_FCT_in;
+            Buff_SEL_FCT_out <= Buff_SEL_FCT_in;
         end if;
     end process;
     
-        Buf_SEL_OUT_process : process (reset, clk)
+        Buff_SEL_OUT_process : process (reset, clk)
     begin
         if reset = '1' then
-            Buf_SEL_OUT_out <= (others => '0'); 
+            Buff_SEL_OUT_out <= (others => '0'); 
         elsif (rising_edge(clk)) then
-            Buf_SEL_OUT_out <= Buf_SEL_OUT_In;
+            Buff_SEL_OUT_out <= Buff_SEL_OUT_In;
         end if;
     end process;
         
